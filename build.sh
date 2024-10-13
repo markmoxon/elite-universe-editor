490bbc26911cfe38d7886513a00fc8773cabd3a2
Mark Moxon <mark@moxon.net>
2024-10-13 13:14:12 +0100

    Update submodules in build

--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ $c1541 \
             d64 \
             3-compiled-game-discs/elite-universe-editor-c64-ntsc.d64 \
     -attach 3-compiled-game-discs/elite-universe-editor-c64-ntsc.d64 \
-    -write c64-elite-universe-editor/work/ntsc/firebird \
-    -write c64-elite-universe-editor/work/ntsc/gma1.modified gma1 \
-    -write c64-elite-universe-editor/work/ntsc/gma3 \
-    -write c64-elite-universe-editor/work/ntsc/gma4 \
-    -write c64-elite-universe-editor/work/ntsc/gma5 \
-    -write c64-elite-universe-editor/work/ntsc/gma6.encrypted gma6 \
+    -write elite-universe-editor-commodore-64/work/ntsc/firebird \
+    -write elite-universe-editor-commodore-64/work/ntsc/gma1.modified gma1 \
+    -write elite-universe-editor-commodore-64/work/ntsc/gma3 \
+    -write elite-universe-editor-commodore-64/work/ntsc/gma4 \
+    -write elite-universe-editor-commodore-64/work/ntsc/gma5 \
+    -write elite-universe-editor-commodore-64/work/ntsc/gma6.encrypted gma6 \
     -write elite-universe-editor-library/universe-files/u.boxart1.bin "boxart1" \
     -write elite-universe-editor-library/universe-files/u.boxart2.bin "boxart2" \
     -write elite-universe-editor-library/universe-files/u.boxartc.bin "boxartc" \
@@ -30,13 +30,13 @@ $c1541 \
             d64 \
             3-compiled-game-discs/elite-universe-editor-c64-pal.d64 \
     -attach 3-compiled-game-discs/elite-universe-editor-c64-pal.d64 \
-    -write c64-elite-universe-editor/work/pal/firebird \
-    -write c64-elite-universe-editor/work/pal/byebyejulie \
-    -write c64-elite-universe-editor/work/pal/gma1.modified gma1 \
-    -write c64-elite-universe-editor/work/pal/gma3 \
-    -write c64-elite-universe-editor/work/pal/gma4 \
-    -write c64-elite-universe-editor/work/pal/gma5 \
-    -write c64-elite-universe-editor/work/pal/gma6.encrypted gma6 \
+    -write elite-universe-editor-commodore-64/work/pal/firebird \
+    -write elite-universe-editor-commodore-64/work/pal/byebyejulie \
+    -write elite-universe-editor-commodore-64/work/pal/gma1.modified gma1 \
+    -write elite-universe-editor-commodore-64/work/pal/gma3 \
+    -write elite-universe-editor-commodore-64/work/pal/gma4 \
+    -write elite-universe-editor-commodore-64/work/pal/gma5 \
+    -write elite-universe-editor-commodore-64/work/pal/gma6.encrypted gma6 \
     -write elite-universe-editor-library/universe-files/u.boxart1.bin "boxart1" \
     -write elite-universe-editor-library/universe-files/u.boxart2.bin "boxart2" \
     -write elite-universe-editor-library/universe-files/u.boxartc.bin "boxartc" \
